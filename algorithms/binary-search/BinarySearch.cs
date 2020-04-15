16952c1dcc1573f8e5e19ec6781e523c9cd7fa0b
Cherepanov Gleb <glcherepanov@yandex.ru>
2020-04-15 16:29:49 +0300

    бинарный поиск

---
+++ algorithms/binary-search/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace binary_search
+{
+    public class BinarySearch
+    {
+        public static int Execute( Dictionary<int, int> dictionary, int item )
+        {
+            int low = 0;
+            int high = dictionary.Count - 1;
+
+            while ( low <= high )
+            {
+                int mid = ( low + high ) / 2;
+                int guess = dictionary[ mid ];
+
+                if ( guess == item )
+                {
+                    return mid;
+                }
+                if ( guess > item )
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ algorithms/binary-search/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace binary_search
+{
+    class Program
+    {
+        private static readonly Dictionary<int, int> _dictionary = new Dictionary<int, int>();
+        private static readonly int DictionarySize = 500;
+
+        static void Main( string[] args )
+        {
+            for ( int i = 0; i < DictionarySize; i++ )
+            {
+                _dictionary.Add( i, i );
+            }
+
+            Console.WriteLine( BinarySearch.Execute( _dictionary, 50 ) );
+        }
+    }
+}
