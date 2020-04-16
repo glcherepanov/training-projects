01725f574a6b67ef62b2194f386e464d323cea28
Cherepanov Gleb <glcherepanov@yandex.ru>
2020-04-16 16:43:40 +0300

    Генератор простых чисел

---
+++ Lab2/prime-numbers/prime-numbers/PrimeNumber.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace prime_numbers
+{
+    public class PrimeNumber
+    {
+        private static readonly int FirstPrimalNumber = 2;
+        private static List<bool> _isPrime = new List<bool>();
+
+        public static HashSet<int> GeneratePrimeNumbersSet( int upperBound )
+        {
+            IsPrimeListInit( upperBound + 1 );
+
+            for ( int i = FirstPrimalNumber; i * i <= upperBound; i++ )
+            {
+                if ( _isPrime[ i ] )
+                {
+                    for ( int noPrime = i * i; noPrime <= upperBound; noPrime += i )
+                    {
+                        _isPrime[ noPrime ] = false;
+                    }
+                }
+            }
+
+            HashSet<int> primeNumbers = new HashSet<int>();
+
+            for (int i = FirstPrimalNumber; i < upperBound; i++ )
+            {
+                if ( _isPrime[ i] )
+                {
+                    primeNumbers.Add( i );
+                }
+            }
+
+            return primeNumbers;
+        }
+
+        private static void IsPrimeListInit( int upperBound )
+        {
+            for ( int i = 0; i < upperBound; i++ )
+            {
+                _isPrime.Add( true );
+            }
+        }
+    }
+}
---
+++ Lab2/prime-numbers/prime-numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prime_numbers
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( PrimeNumber.GeneratePrimeNumbersSet( 100000000 ).Count );
+        }
+    }
+}
